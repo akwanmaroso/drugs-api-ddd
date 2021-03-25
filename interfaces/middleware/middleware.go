b72f373bd021727c332754eee5f0eb3250971d7a
akwanmaroso <akwanmaroso@gmail.com>
2021-03-25 15:25:07 +0800

    feat: add middleware auth

---
+++ interfaces/middleware/middleware.go
@@ -0,0 +1,22 @@
+package middleware
+
+import (
+	"github.com/akwanmaroso/ddd-drugs/infrastructure/auth"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		err := auth.TokenValid(c.Request)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"status": http.StatusUnauthorized,
+				"error":  err.Error(),
+			})
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
